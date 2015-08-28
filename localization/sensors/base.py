0f58eb823508c087997fbcb42cea3d91dc2a4d90
Adnan Ademovic <adnanademovic100@gmail.com>
2015-08-28 21:30:01 +0200

    Add base class for making sensors.
    + Add class that generates measurements based on passed real state
    + Add tests for basic class functionality

---
+++ localization/sensors/base.py
@@ -0,0 +1,28 @@
+class SensorBase(object):
+    """Base class for simulating sensors"""
+    def __init__(self, start_time, delta_time, covariance, measured_values):
+        super(SensorBase, self).__init__()
+        state_size = 15
+        self.covariance = covariance
+        self.next_measurement_time = start_time
+        self.delta_time = delta_time
+        self.measured_values = measured_values
+        self.update_vector = [False] * state_size
+        for i in measured_values:
+            self.update_vector[i] = True
+
+    def getStateMeasurementsUntil(self, real_state, end_time):
+        if self.next_measurement_time > end_time:
+            return None
+        retval = self.generateMeasurement(real_state)
+        retval.time = self.next_measurement_time
+        self.next_measurement_time += self.delta_time
+        if self.next_measurement_time <= end_time:
+            message = 'Sensor needs to be called no less often than every {}s'
+            raise ValueError(message.format(self.delta_time))
+        return retval
+
+    def generateMeasurement(self, real_state):
+        # Leaving measurement generation based on state to be fully open to
+        # using any distribution for generating noise and drifts.
+        raise NotImplementedError("Please Implement this method")
---
+++ tests/localization/sensors/test_base.py
@@ -0,0 +1,54 @@
+import numpy
+import unittest
+
+import localization.sensors.base
+import localization.util
+
+
+class SensorDerived(localization.sensors.base.SensorBase):
+    def __init__(self, test_instance, start_time):
+        covariance = numpy.diag([1.5, 2.])
+        super(SensorDerived, self).__init__(start_time, 1., covariance, [3, 4])
+        self.test_instance = test_instance
+
+    def generateMeasurement(self, real_state):
+        return localization.util.Measurement(
+                0., self.covariance.dot(real_state[3:5, 0:1]),
+                self.covariance, self.update_vector)
+
+
+class TestSensorBase(unittest.TestCase):
+    def test_constructor(self):
+        derived = SensorDerived(self, 10.)
+        self.assertSequenceEqual(
+                [False] * 3 + [True] * 2 + [False] * 10, derived.update_vector)
+        self.assertAlmostEqual(10., derived.next_measurement_time)
+
+    def test_get_state_measurements_until(self):
+        derived = SensorDerived(self, 10.)
+        self.assertAlmostEqual(10., derived.next_measurement_time)
+
+        meas = derived.getStateMeasurementsUntil(
+                numpy.arange(15.).reshape([15, 1]), 10.3)
+        self.assertIsNotNone(meas)
+        self.assertSequenceEqual([2, 1], meas.measurement.shape)
+        self.assertAlmostEqual(4.5, meas.measurement[0, 0])
+        self.assertAlmostEqual(8., meas.measurement[1, 0])
+        self.assertAlmostEqual(11., derived.next_measurement_time)
+
+        meas = derived.getStateMeasurementsUntil(
+                numpy.arange(2., 17.).reshape([15, 1]), 10.6)
+        self.assertIsNone(meas)
+        self.assertAlmostEqual(11., derived.next_measurement_time)
+
+        meas = derived.getStateMeasurementsUntil(
+                numpy.arange(4., 19.).reshape([15, 1]), 11.2)
+        self.assertIsNotNone(meas)
+        self.assertSequenceEqual([2, 1], meas.measurement.shape)
+        self.assertAlmostEqual(10.5, meas.measurement[0, 0])
+        self.assertAlmostEqual(16., meas.measurement[1, 0])
+        self.assertAlmostEqual(12., derived.next_measurement_time)
+
+
+if __name__ == '__main__':
+    unittest.main()
