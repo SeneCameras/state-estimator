a4d872cef7aa3d8df901d8806724bd588d40099c
Adnan Ademovic <adnanademovic100@gmail.com>
2015-09-04 15:48:53 +0200

    Format data more nicely.

--- plotcomparison.py
+++ plotcomparison.py
@@ -16,12 +16,9 @@ if __name__ == '__main__':
                 for vision_variance in (0.1, 0.01, 0.001, 0.0001):
                     cases.append([filter_name, vision_freq, vision_variance, t])
 
-    print 'Red   - Stationary EKF'
-    print 'Blue  - Stationary UKF'
-    print 'Green - Oscillating EKF'
-    print 'Cyan  - Oscillating UKF'
-
     step = len(responses) / 4
+
+    t = numpy.arange(responses[0][0]['position']['real'].shape[1]) * 0.00025
 
     # Compare individual cases between oscillating and stationary, EKF and UKF
     for case in xrange(step):
@@ -29,6 +26,7 @@ if __name__ == '__main__':
         pp.suptitle('Vision freq: {0} Hz; Variance multiplier: {1}'.format(
                 cases[case][1], cases[case][2]))
         subcases = [responses[case + offs] for offs in numpy.arange(4) * step]
+        legend_handles = []
         for subcase, color in zip(subcases, ['red', 'blue', 'green', 'cyan']):
             # Average the datapoints
             p_error = 0.
@@ -38,20 +36,34 @@ if __name__ == '__main__':
             p_error /= len(subcase)
 
             pp.subplot(2, 2, 1)
-            pp.plot(p_error[0, :], color=color)
+            pp.plot(t, p_error[0, :] * 1000., color=color)
             pp.subplot(2, 2, 2)
-            pp.plot(p_error[1, :], color=color)
+            pp.plot(t, p_error[1, :] * 1000., color=color)
             pp.subplot(2, 2, 3)
-            pp.plot(p_error[2, :], color=color)
+            pp.plot(t, p_error[2, :] * 1000., color=color)
             p_error_abs = numpy.sqrt(numpy.sum(p_error * p_error, axis=0))
             pp.subplot(2, 2, 4)
-            pp.plot(p_error_abs, color=color)
+            legend_handles.extend(pp.plot(t, p_error_abs * 1000., color=color))
         pp.subplot(2, 2, 1)
         pp.title('Error along X axis')
+        pp.ylabel('Error (mm)')
         pp.subplot(2, 2, 2)
         pp.title('Error along Y axis')
         pp.subplot(2, 2, 3)
         pp.title('Error along Z axis')
+        pp.ylabel('Error (mm)')
+        pp.xlabel('Time (s)')
         pp.subplot(2, 2, 4)
-        pp.title('Euclidean distance error')
-        pp.show()
+        pp.title('Overall error in H2 norm')
+        pp.xlabel('Time (s)')
+        fig = pp.gcf()
+        fig.set_size_inches(32., 18.)
+        legend_labels = [
+            'Stationary EKF',
+            'Stationary UKF',
+            'Oscillating EKF',
+            'Oscillating UKF',
+        ]
+        pp.figlegend(legend_handles, legend_labels, 'center')
+        fig.savefig('plot_{0}.eps'.format(case))
+        fig.savefig('plot_{0}.png'.format(case))
