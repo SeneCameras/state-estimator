ed5c7a65796d005b82a78fc4abc0e1724fc92970
Adnan Ademovic <adnanademovic100@gmail.com>
2015-09-22 21:38:07 +0200

    Add IMU gyro drift and gravity
    Doesn't break the previous tests, since the defaults are no drift or
    gravity.

--- localization/sensors/imu.py
+++ localization/sensors/imu.py
@@ -2,7 +2,7 @@ import math
 import numpy
 
 import localization.sensors.base
-from localization.util import Measurement, StateMember
+from localization.util import Measurement, StateMember, rpyToRotationMatrix
 
 
 class InvensenseMPU9250(localization.sensors.base.SensorBase):
@@ -12,14 +12,31 @@ class InvensenseMPU9250(localization.sensors.base.SensorBase):
 
     All covariances are set based on the sensor's datasheet.
 
+    Allows setting of optional gyro drift and gravity intensity.
+
     Parameters
     ----------
     start_time: float
         Start time of the sensor usage, in seconds. Preferably set to 0.
     frequency: float
         Frequency of sensor responses, in Hz.
+    gyro_drift: numpy.ndarray, optional
+        Drift of the gyroscope. Default value is no drift. 3x1 array.
+    gravity_drift: float, optional
+        Accelerometer error caused by gravity. Default value is no gravity.
+
+    Attributes
+    ----------
+    gyro_drift
+    gravity_vector: numpy.ndarray
+        Stores vector representing the gravitational force.
     """
-    def __init__(self, start_time, frequency):
+    def __init__(self, start_time, frequency,
+                 gyro_drift = None, gravity_drift = None):
+        if gyro_drift is None:
+            gyro_drift = numpy.zeros([3, 1])
+        if gravity_drift is None:
+            gravity_drift = 0.
         delta_time = 1. / frequency
 
         g = 9.80665
@@ -27,6 +44,10 @@ class InvensenseMPU9250(localization.sensors.base.SensorBase):
         # Based on datasheet:
         npsd_gyro = 0.01 * deg_to_rad
         npsd_accel = 300e-6 * g
+
+        self.gravity_vector = numpy.zeros([3, 1])
+        self.gravity_vector[2, 0] = gravity_drift
+        self.gyro_drift = numpy.array(gyro_drift).reshape([3, 1])
 
         covariance = numpy.diag([npsd_gyro] * 3 + [npsd_accel] * 3) * frequency
         super(InvensenseMPU9250, self).__init__(
@@ -37,7 +58,7 @@ class InvensenseMPU9250(localization.sensors.base.SensorBase):
     def generateMeasurement(self, real_state):
         """Generate an IMU measurement based on the given state.
 
-        Gravity is not added yet to this measurement.
+        Gravity and gyro drift are added to this measurement.
 
         Parameters
         ----------
@@ -49,8 +70,15 @@ class InvensenseMPU9250(localization.sensors.base.SensorBase):
         localization.util.Measurement
             Generate a measurement with added offsets, errors and noises.
         """
+        # Apply noise.
         meas = numpy.asarray(
                 real_state[StateMember.v_roll:StateMember.a_z+1]).reshape(6)
         meas = numpy.random.multivariate_normal(meas, self.covariance)
         meas = numpy.asarray(meas).reshape([6, 1])
+        # Apply gyro drift.
+        meas[0:3] += self.gyro_drift
+        # Apply gravity.
+        rpy = real_state[StateMember.roll:StateMember.yaw+1, 0].tolist()
+        meas[3:6] += rpyToRotationMatrix(*rpy).dot(self.gravity_vector)
+
         return Measurement(0., meas, self.covariance, self.update_vector)
--- tests/localization/sensors/test_imu.py
+++ tests/localization/sensors/test_imu.py
@@ -11,8 +11,30 @@ class TestImu(unittest.TestCase):
         self.assertSequenceEqual([9, 10, 11, 12, 13, 14], imu.update_vector)
         self.assertAlmostEqual(10., imu.next_measurement_time)
         self.assertAlmostEqual(2., imu.delta_time)
+        self.assertSequenceEqual([3, 1], imu.gravity_vector.shape)
+        self.assertAlmostEqual(0., imu.gravity_vector[0, 0])
+        self.assertAlmostEqual(0., imu.gravity_vector[1, 0])
+        self.assertAlmostEqual(0., imu.gravity_vector[2, 0])
+        self.assertSequenceEqual([3, 1], imu.gyro_drift.shape)
+        self.assertAlmostEqual(0., imu.gyro_drift[0, 0])
+        self.assertAlmostEqual(0., imu.gyro_drift[1, 0])
+        self.assertAlmostEqual(0., imu.gyro_drift[2, 0])
 
-    def test_get_state_measurements_until(self):
+        imu = localization.sensors.imu.InvensenseMPU9250(
+                10., 0.5, [1., 2., 3.], 9.2)
+        self.assertSequenceEqual([9, 10, 11, 12, 13, 14], imu.update_vector)
+        self.assertAlmostEqual(10., imu.next_measurement_time)
+        self.assertAlmostEqual(2., imu.delta_time)
+        self.assertSequenceEqual([3, 1], imu.gravity_vector.shape)
+        self.assertAlmostEqual(0., imu.gravity_vector[0, 0])
+        self.assertAlmostEqual(0., imu.gravity_vector[1, 0])
+        self.assertAlmostEqual(9.2, imu.gravity_vector[2, 0])
+        self.assertSequenceEqual([3, 1], imu.gyro_drift.shape)
+        self.assertAlmostEqual(1., imu.gyro_drift[0, 0])
+        self.assertAlmostEqual(2., imu.gyro_drift[1, 0])
+        self.assertAlmostEqual(3., imu.gyro_drift[2, 0])
+
+    def test_get_state_measurements_until_no_drift(self):
         imu = localization.sensors.imu.InvensenseMPU9250(10., 0.5)
         self.assertAlmostEqual(10., imu.next_measurement_time)
 
@@ -55,6 +77,62 @@ class TestImu(unittest.TestCase):
         self.assertAlmostEqual(16., meas.measurement[3, 0])
         self.assertAlmostEqual(17., meas.measurement[4, 0])
         self.assertAlmostEqual(18., meas.measurement[5, 0])
+
+    def test_get_state_measurements_until_with_drift(self):
+        imu = localization.sensors.imu.InvensenseMPU9250(
+                10., 0.5, [1., 2., 3.], 9.81)
+        self.assertAlmostEqual(10., imu.next_measurement_time)
+
+        desired_covariance = numpy.diag(
+                [8.726646259971648e-05] * 3 + [1.4709975e-3] * 3)
+        self.assertSequenceEqual([6, 6], imu.covariance.shape)
+        for i in xrange(6):
+            for j in xrange(6):
+                self.assertAlmostEqual(
+                        desired_covariance[i, j], imu.covariance[i, j])
+
+
+        # Set a zero covariance matrix to make tests deterministic
+        imu.covariance = numpy.zeros([6, 6])
+
+        # Test without tilting
+        meas_val = numpy.arange(15.).reshape([15, 1])
+        meas_val[3:6] = numpy.zeros([3, 1])
+        meas = imu.getStateMeasurementsUntil(meas_val, 10.3)
+        self.assertIsNotNone(meas)
+        self.assertSequenceEqual([6, 1], meas.measurement.shape)
+        self.assertAlmostEqual(10., meas.measurement[0, 0])
+        self.assertAlmostEqual(12., meas.measurement[1, 0])
+        self.assertAlmostEqual(14., meas.measurement[2, 0])
+        self.assertAlmostEqual(12., meas.measurement[3, 0])
+        self.assertAlmostEqual(13., meas.measurement[4, 0])
+        self.assertAlmostEqual(23.81, meas.measurement[5, 0])
+        self.assertAlmostEqual(12., imu.next_measurement_time)
+
+        meas = imu.getStateMeasurementsUntil(
+                numpy.arange(2., 17.).reshape([15, 1]), 11.6)
+        self.assertIsNone(meas)
+        self.assertAlmostEqual(12., imu.next_measurement_time)
+
+        # Test with tilting
+
+        g = numpy.zeros([3, 1])
+        g[2, 0] = 9.81
+        meas = imu.getStateMeasurementsUntil(
+                numpy.arange(15.).reshape([15, 1]), 10.3)
+        g_offs = localization.util.rpyToRotationMatrix(3., 4., 5.).dot(g)
+
+        meas = imu.getStateMeasurementsUntil(
+                numpy.arange(4., 19.).reshape([15, 1]), 12.2)
+        g_offs = localization.util.rpyToRotationMatrix(7., 8., 9.).dot(g)
+        self.assertIsNotNone(meas)
+        self.assertSequenceEqual([6, 1], meas.measurement.shape)
+        self.assertAlmostEqual(14., meas.measurement[0, 0])
+        self.assertAlmostEqual(16., meas.measurement[1, 0])
+        self.assertAlmostEqual(18., meas.measurement[2, 0])
+        self.assertAlmostEqual(16. + g_offs[0, 0], meas.measurement[3, 0])
+        self.assertAlmostEqual(17. + g_offs[1, 0], meas.measurement[4, 0])
+        self.assertAlmostEqual(18. + g_offs[2, 0], meas.measurement[5, 0])
 
 
 if __name__ == '__main__':
